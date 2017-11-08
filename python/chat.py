73a60157508bcd264519f1e18ac17b7b4d35f30f
Richard <richardhayes777@gmail.com>
2017-11-08 16:10:56 +0000

    all comments

--- python/chat.py
+++ python/chat.py
@@ -1,11 +1,48 @@
 import re
+import datetime
+
+DATE_TIME_FORMAT = "%d/%m/%Y, %H:%M:%S"
+
+
+class Item:
+    def __init__(self, line):
+        self.line = line
+
+    @property
+    def datetime(self):
+        date_part = self.line.split(": ")[0]
+        return datetime.datetime.strptime(date_part, DATE_TIME_FORMAT)
+
+    @property
+    def is_comment(self):
+        return len(self.line.split(": ")) > 2 and not self.is_image
+
+    @property
+    def author(self):
+        return self.line.split(": ")[1]
+
+    @property
+    def text(self):
+        return self.line.split(" {}: ".format(self.author))[-1]
+
+    @property
+    def is_image(self):
+        return '<\xe2\x80\x8eimage omitted>\r\n' == self.line.split(" {}: ".format(self.author))[-1]
+
 
 class Chat:
     def __init__(self, filename):
-        self.file = open(filename, 'r+')
+        f = open(filename, 'r+')
+        self.lines = f.readlines()
+        f.close()
 
+    @property
+    def items(self):
+        return map(Item, self.lines)
+
+    @property
     def comments(self):
-        re.compile('\d\d\d\d-\d\d-\d\d\(T\)\d\d:\d\d:\d\d')
+        return filter(lambda i: i.is_comment, self.items)
 
 
 def is_item_start(line):
